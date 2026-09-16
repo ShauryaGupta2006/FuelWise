73884205582b10e596461313be6c3b6c440e9c4a
Shaurya Gupta <shauryakanha2@gmail.com>
2026-09-17 00:20:55 +0530

    first commit

---
+++ View/src/pages/home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+        <div>
+            <h1>FuelWise</h1>
+            <form>
+                <input type="text" placeholder="Odometer Reading" className="rounded-3xl bg-red-500"></input>
+
+            </form>
+        </div>
+    )
+}
+
+export default Home;
---
+++ server/server.js
@@ -0,0 +1,7 @@
+const app = require("./app");
+require("dotenv").config()
+
+
+app.listen(`${process.env.PORT}`,()=>{
+    console.log(`Server Initiated at port ${process.env.PORT}`)
+})
